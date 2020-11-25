e9ca59dc046a6480c2c7c7440a73d3d38f8e5a86
CuseCose <dillemic000@gmail.com>
2020-11-24 23:30:32 -0600

    matrix resolution rescaling

--- Importing.py
+++ Importing.py
@@ -1,3 +1,6 @@
+import math
+
+
 def csvToMatrix(csv):
     lines = []
     cstr = ""
@@ -33,6 +36,34 @@ def reverse(mat):
     return mat1
 
 
+def invert(mat):
+    mat1 = []
+    wid = len(mat)
+    hei = len(mat[0])
+    for y in range(hei):
+        vals = []
+        for x in range(wid):
+            vals.append(mat[x][y])
+        mat1.append(vals)
+    return mat1
+
+
+def resize(mat, s):
+    c = len(mat)
+    mat1 = []
+    for i in range(s):
+        percent = float(i) / s
+        exactind = percent * c
+        decimal = exactind - math.floor(exactind)
+        val = 0
+        if (math.ceil(exactind) >= c):
+            val = mat[math.floor(exactind)]
+        else:
+            val = mat[math.floor(exactind)] * (1 - decimal) + (decimal) * mat[math.ceil(exactind)]
+        mat1.append(val)
+    return mat1
+
+
 def getData(num):
     fname = "dronedata\drone" + str(num)
     lidfname = fname + "lid.txt"
@@ -41,8 +72,20 @@ def getData(num):
     lid_file = open(lidfname, 'r')
     dat_raw = dat_file.read()
     lid_raw = lid_file.read()
-    lid = reverse(csvToMatrix(lid_raw))
-    dat = csvToMatrix(dat_raw)
+    lid = invert(reverse(csvToMatrix(lid_raw)))
+    dat = invert(csvToMatrix(dat_raw))
+    if (len(lid[0]) < len(dat[0])):
+        for i in range(5):
+            lid[i] = resize(lid[i], len(dat[0]))
+    else:
+        for i in range(11):
+            dat[i] = resize(dat[i], len(lid[0]))
     print(lid)
     print(dat)
+    full = []
+    full.append(dat[0])
+    for i in range(4):
+        full.append(lid[i + 1])
+    for i in range(10):
+        full.append(dat[i + 1])
     return [dat, lid]
--- Main.py
+++ Main.py
@@ -1,5 +1,65 @@
 import Importing as imp
+from graphics import *
+import keyboard
+import time
+import sys
 
+height = 800
+width = 1000
+anchorpoint = Point(width / 2, height / 2)
+
+win = GraphWin("map_display_window", width, height)
+image = Image(anchorpoint, height, width)
+
+data = []
 for i in range(5):
-    print("data "+str(i+1)+" : ")
-    imp.getData(i+1)
+    print("data " + str(i + 1) + " : ")
+    data.append(imp.getData(i + 1))
+
+
+def clear(win):
+    for item in win.items[:]:
+        item.undraw()
+    win.update()
+
+
+def renderLid():
+    globals()
+    clear(win)
+    print('drawing')
+    colors = ["green", "red", "blue", "black"]
+    for s in range(5):
+        adj = int(height * float(s + 1) / 5)
+        lid1 = data[s][1]
+        for l in range(4):
+            x = 0
+            y = lid1[0][1 + l]
+            size = len(lid1)
+            for n in range(size):
+                last = Point(x, adj - y * 5)
+                x += width / size
+                y = lid1[n][1 + l]
+                this = Point(x, adj - y * 5)
+                lin = Line(last, this)
+                lin.setFill(colors[l])
+                lin.draw(win)
+            data[s][1] = smooth(data[s][1], l + 1)
+
+
+def smooth(mat, ind):
+    size = len(mat)
+    last = mat[0][ind]
+    newvals = []
+    for i in range(size - 1):
+        v = (last + mat[i + 1][ind]) / 2.0 * .3 + mat[i][ind] * .7
+        last = mat[i][ind]
+        newvals.append(v)
+    for v in range(len(newvals)):
+        mat[v][ind] = newvals[v]
+    return mat
+
+
+'''while not keyboard.is_pressed('esc'):
+    # if (keyboard.is_pressed('r')):
+    renderLid()
+    time.sleep(2)'''
