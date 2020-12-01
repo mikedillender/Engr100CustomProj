6b2c0fbcf05d98f1e1018f8997c7cc65afcbc3df
CuseCose <dillemic000@gmail.com>
2020-11-30 23:35:31 -0500

    reformatting

--- Main.py
+++ Main.py
@@ -55,7 +55,7 @@ def getLidVelAt(mat, n, i):
     return deltas
 
 def getAbberation(mat, n):
-    dt=1
+    dt = 1
     v = 0
     if (n == 0):
         v = abs(mat[n + 1] - mat[n]) / dt
@@ -66,45 +66,45 @@ def getAbberation(mat, n):
         v = (v + abs(mat[n + 1] - mat[n]) / dt) / 2
     return v
 
-def averageAboutN(mat,n,s):
-    sz=len(mat)
-    num=0
-    denom=0
-    powr=1.5
+
+def averageAboutN(mat, n, s):
+    sz = len(mat)
+    num = 0
+    denom = 0
+    powr = 1.5
     for i in range(s):
-        b=n-i
-        f=n+i
-        s=pow(s*((i+1)/(s+1)),powr)
-        if b>=0:
-            num=num+(mat[b]/s)
-            denom=denom+s
-        if f<sz:
-            num=num+(mat[b]/s)
-            denom=denom+s
-    return num/denom
+        b = n - i
+        f = n + i
+        s = pow(s * ((i + 1) / (s + 1)), powr)
+        if b >= 0:
+            num = num + (mat[b] / s)
+            denom = denom + s
+        if f < sz:
+            num = num + (mat[b] / s)
+            denom = denom + s
+    return num / denom
 
 
 def smoothAbberant(mat):
     mat1 = []
-    sz=len(mat)
-    dtype=[('v',float),('i',int)]
+    sz = len(mat)
+    dtype = [('v', float), ('i', int)]
     for i in range(len(mat)):
-        mat1.append((getAbberation(mat, i),i))
-    arr=np.array(mat1,dtype=dtype)
-    #print(arr)
-    arr=np.sort(arr,order='v')
-    tosmooth=int(sz/30)
+        mat1.append((getAbberation(mat, i), i))
+    arr = np.array(mat1, dtype=dtype)
+    # print(arr)
+    arr = np.sort(arr, order='v')
+    tosmooth = int(sz / 30)
     for i in range(tosmooth):
-        #print(arr[sz - 1 - i][0])
-        if(arr[sz-1-i][0]>2):
-            mat[arr[sz-1-i][1]]=averageAboutN(mat,arr[sz-1-i][1],4)
+        # print(arr[sz - 1 - i][0])
+        if (arr[sz - 1 - i][0] > 2):
+            mat[arr[sz - 1 - i][1]] = averageAboutN(mat, arr[sz - 1 - i][1], 4)
         else:
             return False
     return True
-        #smoothAbberant()
-        #print()
-    #print(arr)
-
+    # smoothAbberant()
+    # print()
+    # print(arr)
 
 
 def smooth(mat):
@@ -119,98 +119,109 @@ def smooth(mat):
         mat[v] = newvals[v]
     return mat
 
+
 def addVelocities(mat, d):
     mat1 = [[], [], [], []]
     for i in range(len(mat[0])):
         vels = getLidVelAt(mat, i, d)
         for n in range(4):
             mat1[n].append(vels[n])
-    lim=20
+    lim = 20
     for set in mat1:
-        last=set[0]
+        last = set[0]
         for i in range(len(set)):
-            if(set[i]>lim):
-                set[i]=lim
-            elif(set[i]<-lim):
-                set[i]=-lim
+            if (set[i] > lim):
+                set[i] = lim
+            elif (set[i] < -lim):
+                set[i] = -lim
     for n in range(4):
-        set1=[]
-        #for i in range(len(mat[1])):
+        set1 = []
+        # for i in range(len(mat[1])):
         #    set1.append(getAbberation(mat1[n],i,n))
-        #for t in range(1):
-            #mat1[n]=smooth(mat1[n])
-        #print("smoothing "+str(n))
-        r=True
-        for i in range(70):
-            r=smoothAbberant(mat1[n])
-            if(not r):
-                i=70
-        mat1[n]=smooth(mat1[n])
+        # for t in range(1):
+        # mat1[n]=smooth(mat1[n])
+        # print("smoothing "+str(n))
+        r = True
+        for i in range(60):
+            r = smoothAbberant(mat1[n])
+            if (not r):
+                i = 70
+        mat1[n] = smooth(mat1[n])
 
         mat.append(mat1[n])
-        #mat.append(set1)
+        # mat.append(set1)
 
-def getNetVel(mat,d,i): # d=0 : front / back, d=1 : right / left
+
+def getNetVel(mat, d, i):  # d=0 : front / back, d=1 : right / left
     ld = [mat[1 + d][i], mat[3 + d][i]]
     lv = [-mat[15 + d][i], mat[17 + d][i]]
-    weight=.5
-    if(lv[0]==0):
+    weight = .5
+    if (lv[0] == 0):
         return lv[1]
-    elif(lv[1]==0):
+    elif (lv[1] == 0):
         return lv[0]
-    return (lv[0]+lv[1])/2
+    return (lv[0] + lv[1]) / 2
+
 
 def constructNetVels():
     for mat in trials:
-        sz=len(mat[0])
+        sz = len(mat[0])
         for d in range(2):
-            d1=[]
+            d1 = []
             for i in range(sz):
-                d1.append(getNetVel(mat,d,i))
+                d1.append(getNetVel(mat, d, i))
             mat.append(d1)
 
+
 def getPositions():
-    t=0
-    scales=[7,10,10,8,10]
+    t = 0
+    scales = [7, 10, 10, 8, 10]
     for mat in trials:
-        sz=len(mat[0])
+        sz = len(mat[0])
         for d in range(2):
-            d1=[]
-            x=0
+            d1 = []
+            x = 0
             for i in range(sz):
-                dt0=.01
+                dt0 = .01
                 d1.append(x)
-                x=x+mat[19+d][i]*dts[t]*scales[t]
-                #x=x+mat[19+d][i]*mat[0][i]*dts[t]*30
+                x = x + mat[19 + d][i] * dts[t] * scales[t]
+                # x=x+mat[19+d][i]*mat[0][i]*dts[t]*30
             d1.append(x)
             mat.append(d1)
-        t+=1
-lps=[]
+        t += 1
+
+
+lps = []
+
+
 def getLidPositions():
     global lps
     t = 0
-    scales=[7.5,6.8,6,7,6]
-    mults=[[1,0],[0,1],[-1,0],[0,-1]]
+    scales = [7.5, 6.8, 6, 7, 6]
+    mults = [[1, 0], [0, 1], [-1, 0], [0, -1]]
     for mat in trials:
         sz = len(mat[0])
-        ps=[]
+        ps = []
         for i in range(sz):
-            dr=Point(mat[21][i],mat[22][i])
+            dr = Point(mat[21][i], mat[22][i])
             for d in range(4):
-                if mat[1+d][i]<14:
-                    ps.append(Point(dr.x + mat[1+d][i] * mults[d][0] * scales[t], dr.y + mat[1+d][i] * mults[d][1]*scales[t]))
+                if mat[1 + d][i] < 14:
+                    ps.append(Point(dr.x + mat[1 + d][i] * mults[d][0] * scales[t],
+                                    dr.y + mat[1 + d][i] * mults[d][1] * scales[t]))
         lps.append(ps)
         t += 1
+
 
 trials = []
 for i in range(5):
     trials.append(imp.getData(i + 1))
 for d in range(5):
     addVelocities(trials[d], d)
-    #print(trials[d])
+    # print(trials[d])
 constructNetVels()
 getPositions()
 getLidPositions()
+
 
 def clear(win):
     for item in win.items[:]:
@@ -222,30 +233,30 @@ def renderLid():
     globals()
     clear(win)
     print('drawing')
-    colors = ["green", "red", "blue", "black","brown"]
-    adjxs=[width/4,width-width/4,width/2,width/4,width-width/4,]
-    adjys=[height/3-100,height/3-100,height/2-50,2*height/3,2*height/3]
+    colors = ["green", "red", "blue", "black", "brown"]
+    adjxs = [width / 4, width - width / 4, width / 2, width / 4, width - width / 4, ]
+    adjys = [height / 3 - 100, height / 3 - 100, height / 2 - 50, 2 * height / 3, 2 * height / 3]
     scale = 1
     for s in range(5):
         adjy = adjys[s] + 100
         adjx = adjxs[s] - 100
         for p in lps[s]:
-            pt=Point(p.x+adjx,p.y+adjy)
+            pt = Point(p.x + adjx, p.y + adjy)
             pt.setFill(colors[s])
             pt.draw(win)
     for s in range(5):
-        #adjy = int(height * float(s + 1) / 6)
-        #adjx = int(width/2)
-        adjy = adjys[s]+100
-        adjx = adjxs[s]-100
+        # adjy = int(height * float(s + 1) / 6)
+        # adjx = int(width/2)
+        adjy = adjys[s] + 100
+        adjx = adjxs[s] - 100
         x = trials[s][21][0]
         y = trials[s][22][0]
         size = len(trials[s][21])
         for n in range(size):
-            last = Point(adjx + x*scale, adjy + y * scale)
+            last = Point(adjx + x * scale, adjy + y * scale)
             x = trials[s][21][n]
             y = trials[s][22][n]
-            this = Point(adjx + x*scale, adjy + y * scale)
+            this = Point(adjx + x * scale, adjy + y * scale)
             lin = Line(last, this)
             lin.setFill(colors[s])
             lin.draw(win)
